f1ddaa7c09d12f2f2c7f6a764e58fcd94053c914
Sean Unland <sdunland@gmail.com>
2025-05-05 18:09:32 -0400

    moved files to repo root

--- src/Views/Main-View/index.tsx
+++ src/Views/Main-View/index.tsx
@@ -1,11 +1,9 @@
 export const MainView = () => {
   return (
-    <div className='flex flex-col items-center justify-center h-screen bg-gray-100'>
+    <div className='flex flex-col items-center justify-center h-screen w-screen bg-orange-300'>
       <h1 className='text-4xl font-bold mb-4'>Welcome to the Main View</h1>
-      <p className='text-lg text-gray-700'>
-        This is the main view of your application.
-      </p>
+      <p className='text-lg text-gray-700'>You are using your template</p>
     </div>
-  );
-};
+  )
+}
 // This is a simple React component that serves as the main view of your application.
